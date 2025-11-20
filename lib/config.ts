94e2b333de62ff107199c62fb9b1095912f1ae98
alineitgirl <queenalina2005@gmail.com>
2025-11-20 11:13:30 +0300

    feat: Add database schema and migrations

--- lib/config.ts
+++ lib/config.ts
@@ -6,7 +6,7 @@ const config = {
             privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
             urlEndpoint: process.env.NEXT_IMAGEKIT_URL_ENDPOINT!
         },
-        databaseUrl: process.env.DATABASE_URL
+        databaseUrl: process.env.DATABASE_URL!
     }
 }
 
