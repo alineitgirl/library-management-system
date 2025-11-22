df97f53bf0f2c905cc3b2fb284513756bd032082
alineitgirl <queenalina2005@gmail.com>
2025-11-22 11:32:02 +0300

    feat: add ratelimiter with upstash/redis

--- lib/config.ts
+++ lib/config.ts
@@ -6,7 +6,11 @@ const config = {
             privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
             urlEndpoint: process.env.NEXT_IMAGEKIT_URL_ENDPOINT!
         },
-        databaseUrl: process.env.DATABASE_URL!
+        databaseUrl: process.env.DATABASE_URL!,
+        upstash: {
+            redisUrl: process.env.UPSTASH_REDIS_REST_URL!,
+            redisToken: process.env.UPSTASH_REDIS_REST_TOKEN!
+        }
     }
 }
 
